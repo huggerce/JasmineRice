7ec4dbd25c5e21661bd8e4576b596307b94cd83d
huggerce <huggerce@vt.edu>
2018-03-13 16:38:15 -0400

    Class Assignment part h

--- L15/opi.c
+++ L15/opi.c
@@ -2,20 +2,30 @@
 #include <stdlib.h>
 #include <math.h>
 
+#include <omp.h>
+
 int main(int argc, char **argv) {
 
   //seed random number generator
   // Q2b: get the number of threads to run with from agrv and 
   // add OpenMP API code to set number of threads here
-  int Nthreads = 1;
+  int Nthreads = atoi(argv[1]);
+
+	//tell OpenMP to use NumThreads threads
+  omp_set_num_threads(Nthreads);
   
   struct drand48_data *drandData; 
   drandData = (struct drand48_data*) malloc(Nthreads*sizeof(struct drand48_data));
 
+	int rank = omp_get_thread_num();	
+
   // Q2c: add an OpenMP parallel region here, wherein each thread initializes 
   //      one entry in drandData using srand48_r and seed based on thread number
-  long int seed = 0;
-  srand48_r(seed, drandData+0);
+  long int seed = rank; //sets the seed based on the thread number
+  srand48_r(seed, drandData+rank);
+
+
+	
 
   long long int Ntrials = 10000000;
 
@@ -24,13 +34,17 @@ int main(int argc, char **argv) {
   long long int Ntotal=0;
   long long int Ncircle=0;
 
+	double startTime = omp_get_wtime();
+	
+	#pragma omp parallel for reduction(+:Ncircle)
   for (long long int n=0; n<Ntrials; n++) {
     double rand1;
     double rand2;
 
+		int rank = omp_get_thread_num();
     //gererate two random numbers (use the thread id to offset drandData)
-    drand48_r(drandData+0, &rand1);
-    drand48_r(drandData+0, &rand2);
+    drand48_r(drandData+rank, &rand1);
+    drand48_r(drandData+rank, &rand2);
     
     double x = -1 + 2*rand1; //shift to [-1,1]
     double y = -1 + 2*rand2;
@@ -47,7 +61,8 @@ int main(int argc, char **argv) {
 
   double pi = 4.0*Ncircle/ (double) (Ntotal);
   printf("Our final estimate of pi is %g \n", pi);
-
+	
+	double endTime = omp_get_wtime();
   free(drandData);
   
   return 0;
