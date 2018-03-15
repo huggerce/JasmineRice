22d55974e040cb820e41d827f21497a5c42a122c
huggerce <huggerce@vt.edu>
2018-03-15 16:02:58 -0400

    question 2e

--- L16/mandelbrot.c
+++ L16/mandelbrot.c
@@ -20,8 +20,10 @@ To create an image with 4096 x 4096 pixels (last argument will be used to set nu
 #include <stdlib.h>
 #include "png_util.h"
 
-// Q2a: add include for OpenMP header file here:
 
+
+// Q2a: add include for OpenMP header file here:
+#include <omp.h>
 
 #define MXITER 1000
 
@@ -70,6 +72,7 @@ void  mandelbrot(int Nre, int Nim, complex_t cmin, complex_t cmax, float *count)
   double di = (cmax.i-cmin.i)/(Nim-1);;
 
   // Q2c: add a compiler directive to split the outer for loop amongst threads here
+	#pragma omp parallel for private(m, c)
   for(n=0;n<Nim;++n){
     for(m=0;m<Nre;++m){
       c.r = cmin.r + dr*m;
@@ -94,6 +97,8 @@ int main(int argc, char **argv){
 
   // Q2b: set the number of OpenMP threads to be Nthreads here:
 
+	omp_set_num_threads(Nthreads);
+
   // storage for the iteration counts
   float *count = (float*) malloc(Nre*Nim*sizeof(float));
 
@@ -111,12 +116,13 @@ int main(int argc, char **argv){
 
   // Q2d: complete this to read time before calling mandelbrot with OpenMP API wall clock time
   double start;
+	start = omp_get_wtime();
 
   // compute mandelbrot set
   mandelbrot(Nre, Nim, cmin, cmax, count); 
   
   // Q2d: complete this to read time after calling mandelbrot using OpenMP wall clock time
-  double end;
+  double end = omp_get_wtime();
   
   // print elapsed time
   printf("elapsed = %g\n", end-start);
