08e46da70c32b2c37cc897279ed3b27ae788cc1a
huggerce <huggerce@vt.edu>
2018-03-28 16:40:57 -0400

    updating main.c again

--- HW03/Part2/main.c
+++ HW03/Part2/main.c
@@ -30,7 +30,7 @@ int main (int argc, char **argv) {
 	//comment this so i can do parallel
   	//printf("Enter a number of bits: "); fflush(stdout);
   	//char status = scanf("%u",&n);
-	n = 4;
+	n = 3;
   	//make sure the input makes sense
   	if ((n<3)||(n>31)) {//Updated bounds. 2 is no good, 31 is actually ok
     	printf("Unsupported bit size.\n");
